8c0f08cb916aea383e0ddade12ee91327bc84573
Matt Layman <matthewlayman@gmail.com>
2022-05-19 18:56:48 -0400

    complete dragonsnax with sphinx

--- ReturnToDragonSnax.py
+++ ReturnToDragonSnax.py
@@ -1,3 +1,7 @@
+import random
+from re import S
+
+
 print("You are in a dark room in a mysterious castle.")
 print("In front of you are four doors. You must choose one.")
 player_choice = input("Choose 1, 2, 3, or 4... ")
@@ -23,6 +27,21 @@ elif player_choice == "3":
         print("GAME OVER! YOU WIN!")
     else:
         print("Sorry, you didn't enter 1 or 2!")
-
+elif player_choice == "4":
+    print("You enter a room with a sphinx.")
+    print("It asks you to guess what number it is thinking of, between 1 and 10.")
+    number = int(input("What number do you choose? "))
+    sphinx_choice = random.randint(1, 2)
+    if number == sphinx_choice:
+        print("The sphinx hisses in disappointment. You guessed correctly.")
+        print("She must let you go free.")
+        print("GAME OVER! YOU WIN!")
+    else:
+        print(
+            "The sphinx tells you that your guess is incorrect. The sphinx chose",
+            sphinx_choice,
+        )
+        print("You are now her prisoner forever.")
+        print("GAME OVER! YOU LOSE.")
 else:
     print("Run the game again to try again.")
