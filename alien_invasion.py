19f9bd5512cc0d31e6420fdf873f205c9623cff9
Matt Layman <matthewlayman@gmail.com>
2022-06-05 18:32:56 -0400

    Add alien invasion

---
+++ alien_invasion.py
@@ -0,0 +1,13 @@
+aliens = 2
+password = "ALIENS"
+print("Quickly! Aliens are invading the planet.")
+print("You need to activate the global defense platforms.")
+print("Hope you know the password, for Earth's sake...")
+print()
+print("---------------------------------------------------------")
+print("         WELCOME TO THE GLOBAL DEFENSE NETWORK           ")
+print("---------------------------------------------------------")
+print()
+
+guess = input("Please enter the password: ").upper()
+# print(guess)
